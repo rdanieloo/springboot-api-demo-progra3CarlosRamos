3f9a1902de48260b2b421091417eaf8c319ad4c6
carlos <carlosramos352m@gmail.com>
2026-04-09 21:25:26 -0600

    Paso 2 - Aca habilite endpoint GET /saludos

--- src/main/java/com/ejemplo/demo/api/controller/SaludoController.java
+++ src/main/java/com/ejemplo/demo/api/controller/SaludoController.java
@@ -1,8 +1,16 @@
+//Programacion3
+//CarlosRam0s
+//09052314141
+
+
 package com.ejemplo.demo.api.controller;
 
+import com.ejemplo.demo.api.dto.SaludoResponse;
+import com.ejemplo.demo.domain.service.SaludoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -10,6 +18,12 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1")
 public class SaludoController {
+
+    private final SaludoService saludoService;
+
+    public SaludoController(SaludoService saludoService) {
+        this.saludoService = saludoService;
+    }
 
     @GetMapping
     public ResponseEntity<Map<String, String>> health() {
@@ -19,50 +33,11 @@ public class SaludoController {
         ));
     }
 
-    /*
-    ===========================================
-    PASO 2: DESCOMENTA este bloque y prueba GET
-    ===========================================
-
-    1) Descomenta imports:
-       - com.ejemplo.demo.api.dto.SaludoResponse
-       - com.ejemplo.demo.domain.service.SaludoService
-       - org.springframework.web.bind.annotation.RequestParam
-
-    2) Descomenta el campo y constructor:
-
-    private final SaludoService saludoService;
-
-    public SaludoController(SaludoService saludoService) {
-        this.saludoService = saludoService;
-    }
-
-    3) Descomenta este endpoint:
-
+    // PASO 2: Endpoint GET /saludos
     @GetMapping("/saludos")
     public ResponseEntity<SaludoResponse> saludar(
             @RequestParam(defaultValue = "Mundo") String nombre
     ) {
         return ResponseEntity.ok(saludoService.crearSaludo(nombre));
     }
-    */
-
-    /*
-    ============================================
-    PASO 3: DESCOMENTA este bloque y prueba POST
-    ============================================
-
-    1) Descomenta imports:
-       - com.ejemplo.demo.api.dto.SaludoRequest
-       - jakarta.validation.Valid
-       - org.springframework.web.bind.annotation.PostMapping
-       - org.springframework.web.bind.annotation.RequestBody
-
-    2) Descomenta este endpoint:
-
-    @PostMapping("/saludos")
-    public ResponseEntity<SaludoResponse> saludarPost(@Valid @RequestBody SaludoRequest request) {
-        return ResponseEntity.ok(saludoService.crearSaludo(request.nombre()));
-    }
-    */
 }
